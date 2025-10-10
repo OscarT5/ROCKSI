09c8cafe358664476f25beb9b030c57fadc5a563
bilileo <l2100754@gmail.com>
2025-10-09 23:26:28 -0700

    PBI-GCLI-US2 (MERGE TESTEADO)

--- entidad/src/main/java/mx/desarollo/entity/Cliente.java
+++ entidad/src/main/java/mx/desarollo/entity/Cliente.java
@@ -1,45 +1,66 @@
 package mx.desarollo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.persistence.*;
 
-import java.time.LocalDate;
+import java.util.*;
 
 @Entity
 @Table(name = "cliente")
 public class Cliente {
+
     @Id
-    @Size(max = 45)
-    @Column(name = "ID_Cliente", nullable = false, length = 45)
+    @Column(name = "ID_Cliente", length = 45)
     private String idCliente;
 
-    @Size(max = 100)
-    @NotNull
     @Column(name = "nombreCompleto", nullable = false, length = 100)
     private String nombreCompleto;
 
-    @Size(max = 15)
-    @NotNull
-    @Column(name = "telefono", nullable = false, length = 15)
+    @Column(name = "telefono", nullable = false, unique = true, length = 15)
     private String telefono;
 
-    @NotNull
-    @Column(name = "credito", nullable = false)
-    private Long credito;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "fechaRegistro")
+    private Date fechaRegistro;
 
-    @NotNull
-    @Column(name = "fechaRegistro", nullable = false)
-    private LocalDate fechaRegistro;
+    /*
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_Membresia")
+    private Membresia membresia;
+    */
 
-    @Size(max = 45)
-    @NotNull
-    @Column(name = "ID_Membresia", nullable = false, length = 45)
-    private String idMembresia;
+     /*
+     @OneToMany(mappedBy = "cliente", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Pago> historialPagos = new ArrayList<>();
 
+    @ManyToMany
+    @JoinTable( //Aqui se hace un join para realizar la tabla puente de estainscrito dentro de la BD
+            name = "estainscrito",
+            joinColumns = @JoinColumn(name = "ID_Cliente"),
+            inverseJoinColumns = @JoinColumn(name = "ID_Clase")
+    )
+    private List<Clase> clases = new ArrayList<>();
+      */
+
+
+
+    @Column(name = "credito")
+    private double credito;
+
+    //constructores
+    public Cliente() {
+        this.idCliente = UUID.randomUUID().toString();//Esto asigna un id aleatorio, MODIFICAR DESPUES
+        this.fechaRegistro = new Date();//Asigna el dia de hoy
+    }
+
+    public Cliente(String nombreCompleto, String telefono, Date fechaRegistro, double credito) {
+        this.nombreCompleto = nombreCompleto;
+        this.telefono = telefono;
+        this.fechaRegistro = fechaRegistro;
+        //this.membresia = membresia;
+        this.credito = credito;
+    }
+
+    //getters y setters
     public String getIdCliente() {
         return idCliente;
     }
@@ -64,28 +85,54 @@ public class Cliente {
         this.telefono = telefono;
     }
 
-    public Long getCredito() {
-        return credito;
-    }
-
-    public void setCredito(Long credito) {
-        this.credito = credito;
-    }
-
-    public LocalDate getFechaRegistro() {
+    public Date getFechaRegistro() {
         return fechaRegistro;
     }
 
-    public void setFechaRegistro(LocalDate fechaRegistro) {
+    public void setFechaRegistro(Date fechaRegistro) {
         this.fechaRegistro = fechaRegistro;
     }
 
-    public String getIdMembresia() {
-        return idMembresia;
+    /*
+    public Membresia getMembresia() {
+        return membresia;
     }
 
-    public void setIdMembresia(String idMembresia) {
-        this.idMembresia = idMembresia;
+    public void setMembresia(Membresia membresia) {
+        this.membresia = membresia;
+    }
+
+    public List<Pago> getHistorialPagos() {//Se cambio el nombre de la clase a "Pagos"
+        return historialPagos;
+    }
+
+    public void setHistorialPagos(List<Pago> historialPagos) {
+        this.historialPagos = historialPagos;
+    }
+
+    public List<Clase> getClases() {
+        return clases;
+    }
+
+    public void setClases(List<Clase> clases) {
+        this.clases = clases;
+    }
+     */
+
+    public double getCredito() {
+        return credito;
+    }
+
+    public void setCredito(double credito) {
+        this.credito = credito;
+    }
+
+    @Transient
+    public String getApellido() {
+        if(nombreCompleto != null && nombreCompleto.contains(" ")) {
+            return nombreCompleto.substring(nombreCompleto.indexOf(' ') + 1);
+        }
+        return "";
     }
 
 }
