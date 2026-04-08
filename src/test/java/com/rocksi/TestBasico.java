3be15ea6b0ec398800b05ea7c3c3df59aae4a2df
bilileo <l2100754@gmail.com>
2026-04-08 14:34:48 -0700

    1st commit Test

---
+++ src/test/java/com/rocksi/TestBasico.java
@@ -0,0 +1,11 @@
+package com.rocksi;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestBasico {
+
+    @Test
+    void pruebaBasica() {
+        assertEquals(2, 1 + 1);
+    }
+}
