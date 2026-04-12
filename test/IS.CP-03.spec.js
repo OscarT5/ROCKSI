d585a55f3f12c634ea3a7b7dd75a83d58095f2aa
bilileo <l2100754@gmail.com>
2026-04-11 18:53:40 -0700

    2nd Commit Inicio de sesion

--- test/IS.CP-03.spec.js
+++ test/IS.CP-03.spec.js
@@ -1,27 +1,26 @@
 const { test, expect } = require('@playwright/test');
 
-test('login ROCKSI', async ({ page }) => {
+test('login ROCKSI incorrecto', async ({ page }) => {
 
     await page.goto('http://localhost:8080/');
 
+    // Detectar CI
     if (process.env.CI) {
         console.log("Modo CI: validando carga básica");
 
-        // Solo validar que cargó algo
         await expect(page).toHaveTitle(/./);
-
         return;
     }
 
-    // Local e BD
+    // SOLO LOCAL
     await page.waitForSelector('[id$="usuario"]', { timeout: 20000 });
 
-    await page.fill('[id$="usuario"]', 'ADM1000');
-    await page.fill('[id$="contrasena"]', '123');
+    await page.fill('[id$="usuario"]', '0000');
+    await page.fill('[id$="contrasena"]', '0000');
 
     await page.click('text=Iniciar sesión');
 
-    await page.waitForURL('**/home.xhtml', { timeout: 20000 });
+    await page.waitForTimeout(3000);
 
-    await expect(page).toHaveURL(/home.xhtml/);
+    await expect(page).not.toHaveURL(/home.xhtml/);
 });
