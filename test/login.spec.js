fc3896692e7fb518c1b631f41266ab93f548d7f9
bilileo <l2100754@gmail.com>
2026-04-11 17:10:18 -0700

    INICIO DE SESION - CP-01

--- test/login.spec.js
+++ test/login.spec.js
@@ -1,7 +1,29 @@
 // tests/login.spec.js
 const { test, expect } = require('@playwright/test');
 
-test('abrir pagina', async ({ page }) => {
-    await page.goto('http://localhost:8080');
-    await expect(page).toHaveTitle(/ROCKSI/);
+test('login correcto', async ({ page }) => {
+
+    // Entrar al login
+    await page.goto('http://localhost:8080/vista/');
+
+    // Ingresar al Usuario
+    await page.fill('[id="loginForm:usuario"]', 'ADM1000');
+
+    // Ingresar contraseña
+    await page.fill('[id="loginForm:contrasena"]', '123');
+
+    // Clic en boton para inicar sesion
+    await page.click('text=Iniciar sesión');
+
+    // Esperar cambio
+    await page.waitForTimeout(3000);
+
+    // Pruebas de Inicio de sesion - CP-01
+
+    // OPCIÓN A: validar cambio de URL
+    await expect(page).not.toHaveURL('http://localhost:8080/vista/home.xhtml');
+
+    // OPCIÓN B: validar que ya no estás en login
+    //await expect(page.locator('text=Inicio de sesion')).not.toBeVisible();
+
 });
