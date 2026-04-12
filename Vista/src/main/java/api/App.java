91fc8095c689ba057e4d452ee92a156fb2582fec
Oscar Tan Guan <oscar.tan@uabc.edu.mx>
2026-04-12 15:22:47 -0700

    Ajustar Esperar servidor

--- Vista/src/main/java/api/App.java
+++ Vista/src/main/java/api/App.java
@@ -2,7 +2,6 @@ package api;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("api")
 public class App extends Application {
     // the entry class - deliberately empty in this basic demo
 }
